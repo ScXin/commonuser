2dd8d04c69ee4fc7baec1520deaceb0006c8d560
ScXin <980425123@qq.com>
2020-07-15 12:50:42 +0800

    最终版

---
+++ src/main/java/com/commonuser/entity/WhiteIP.java
@@ -0,0 +1,31 @@
+package com.commonuser.entity;
+
+/**
+ * @author ScXin
+ * @date 7/14/2020 12:13 PM
+ */
+public class WhiteIP {
+
+    private int id;
+
+    private String addr;
+
+    public WhiteIP() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+}
